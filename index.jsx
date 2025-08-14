405917b25cc2d4baaa9b6e6ab4c5c9df9e7f6924
Leanne-George <leannegeorge9b@gmail.com>
2025-08-14 06:57:59 +0530

    Create index.jsx
    add in pages/index.jsx

---
+++ index.jsx
@@ -0,0 +1,9 @@
+import WeatherWidget from "../components/WeatherWidget";
+
+export default function Home() {
+  return (
+    <div className="p-6 flex justify-center">
+      <WeatherWidget />
+    </div>
+  );
+}
